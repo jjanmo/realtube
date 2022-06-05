4dabad65f652f1ea6d19ddbc61dd271cb654c428
jjanmo <jjanmo07@hanmail.net>
2022-06-05 23:20:34 +0900

    feat: add search bar in navbar

--- front/src/components/Navigation/Navigation.tsx
+++ front/src/components/Navigation/Navigation.tsx
@@ -1,5 +1,6 @@
-import { Grid, GridItem } from '@chakra-ui/react'
+import { Grid, GridItem, Input, InputGroup, InputRightElement } from '@chakra-ui/react'
 import StyledLink from '@components/StyledLink'
+import { SearchIcon } from '@chakra-ui/icons'
 
 const Navigation = () => {
   const activeLink = {
@@ -7,7 +8,7 @@ const Navigation = () => {
   }
 
   return (
-    <Grid templateColumns="repeat(3, 1fr)" gap={10} width={'100%'} h={50} mx="auto" bg="blue.400">
+    <Grid templateColumns="repeat(4, 1fr)" gap={10} width={'100%'} h={50} mx="auto" bg="blue.400">
       <GridItem>
         <StyledLink href="/" text="home" color="white" _hover={activeLink} />
       </GridItem>
@@ -16,6 +17,20 @@ const Navigation = () => {
       </GridItem>
       <GridItem>
         <StyledLink href="/signup" text="signup" color="white" _hover={activeLink} />
+      </GridItem>
+      <GridItem alignSelf="center" mx={5}>
+        <InputGroup>
+          <Input
+            variant="outline"
+            placeholder="Search"
+            _placeholder={{ color: 'white' }}
+            focusBorderColor={'white'}
+            color="white"
+          />
+          <InputRightElement pointerEvents="none">
+            <SearchIcon w={6} h={6} color="white" />
+          </InputRightElement>
+        </InputGroup>
       </GridItem>
     </Grid>
   )
