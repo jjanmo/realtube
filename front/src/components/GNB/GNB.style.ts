8dfb0696ff360d0024bef936d6eaf5bebb9b0980
jjanmo <jjanmo07@hanmail.net>
2023-03-03 16:26:24 +0900

    feat: add gnb

--- front/src/components/GNB/GNB.style.ts
+++ front/src/components/GNB/GNB.style.ts
@@ -1,19 +1,96 @@
+import { onLargeWeb } from '@styles/responsive'
+import { COLORS } from '@constants/colors'
 import styled from 'styled-components'
 
 export const Container = styled.header`
-  width: 100%;
-  height: 7rem;
-  color: white;
-  background-color: #29ab87;
-`
-export const Content = styled.div`
-  max-width: 60rem;
-  height: 100%;
-  margin: auto;
-
   display: flex;
-  justify-content: space-around;
+  flex-wrap: wrap;
+  justify-content: center;
+
+  ${onLargeWeb} {
+    height: 7rem;
+  }
+`
+
+export const LogoWrapper = styled.div`
+  width: 50%;
+  height: 5rem;
+  padding: 0 1rem;
+  flex: 1;
+  display: flex;
+
   align-items: center;
 
-  font-size: 2rem;
+  .logo-text {
+    margin-left: 0.8rem;
+    font-size: 2rem;
+    font-weight: 700;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    color: ${COLORS.GRAY};
+  }
+
+  svg {
+    font-size: 2rem;
+  }
+
+  ${onLargeWeb} {
+    width: 25%;
+    height: 100%;
+    padding: 0 3rem;
+    justify-content: center;
+    order: 1;
+
+    svg {
+      font-size: 3rem;
+    }
+
+    .logo-text {
+      margin-left: 1.2rem;
+      font-size: 2.5rem;
+    }
+  }
+`
+
+export const ButtonContainer = styled.div`
+  width: 50%;
+  height: 5rem;
+  padding: 0 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+
+  button {
+    width: 8rem;
+  }
+
+  .signin {
+    border: 1px solid ${COLORS.THEME_600};
+    color: ${COLORS.THEME_600};
+  }
+  .signup {
+    margin-left: 1rem;
+    background-color: ${COLORS.THEME_500};
+  }
+
+  ${onLargeWeb} {
+    width: 25%;
+    height: 100%;
+    padding: 0 3rem;
+    order: 3;
+  }
+`
+
+export const Navigation = styled.ul`
+  width: 100%;
+  height: 5rem;
+
+  display: flex;
+  align-items: center;
+  font-size: 1.8rem;
+
+  ${onLargeWeb} {
+    width: 50%;
+    height: 100%;
+    order: 2;
+  }
 `
--- front/src/components/GNB/GNB.tsx
+++ front/src/components/GNB/GNB.tsx
@@ -1,15 +1,35 @@
-import StyledLink from '@components/StyledLink'
 import * as S from './GNB.style'
+import Link from 'next/link'
+import { Button } from 'antd'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faKiwiBird, faSeedling } from '@fortawesome/free-solid-svg-icons'
+import { COLORS } from '@constants/colors'
 import { links } from '@constants/links'
 
 const GNB = () => {
   return (
     <S.Container>
-      <S.Content>
+      <S.LogoWrapper>
+        <FontAwesomeIcon icon={faSeedling} color={COLORS.THEME_400} />
+        <div className="logo-text">RealTube</div>
+      </S.LogoWrapper>
+
+      <S.ButtonContainer>
+        <Button className="signin">
+          <Link href="/account/signin">로그인</Link>
+        </Button>
+        <Button className="signup" type="primary">
+          <Link href="/account/signup">가입하기</Link>
+        </Button>
+      </S.ButtonContainer>
+
+      <S.Navigation>
         {links.map(({ text, href }) => (
-          <StyledLink key={href} href={href} text={text} />
+          <Button key={text} type="text">
+            <Link href={href}>{text}</Link>
+          </Button>
         ))}
-      </S.Content>
+      </S.Navigation>
     </S.Container>
   )
 }
